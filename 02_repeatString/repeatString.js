663aeb9285a869c937d03e80554b612d150ba89e
clairemanthey <clairemanthey@gmail.com>
2022-11-01 16:16:05 -0500

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	let newString = "";
+	while (num > 0){
+		num = num - 1;
+		newString = newString + string;
+	}
+	return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
