86c995244e29aa40041ad9dae626156801fc5499
clairemanthey <clairemanthey@gmail.com>
2022-11-02 15:28:26 -0500

    added my solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
+//limit to 1 decimal place
 
+const ftoc = function(farValue) {
+  let cel = (farValue - 32) * (5/9)
+  let celVal = Math.round(cel * 10) / 10;
+  return celVal
 };
 
-const ctof = function() {
 
+const ctof = function(celValue) {
+  let far = celValue * 9/5 + 32
+  let farVal = Math.round(far * 10) / 10;
+  return farVal;
 };
+
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
