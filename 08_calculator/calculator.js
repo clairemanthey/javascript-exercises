872490f9bce0a3a5adac9169b6e9ea75f95eeda9
clairemanthey <clairemanthey@gmail.com>
2022-11-02 22:46:24 -0500

    added my solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+    return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const power = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	let re = /[^A-Za-z0-9]/g;
+	str = str.toLowerCase().replace(re, "");
+	let strLength = str.length;
+	for(let i =  0; i < strLength / 2; i++){
+		if(str[i] !== str[strLength - 1 -i]){
+			return false
+		}
+	}
+	return true;
+
 
 };
 
