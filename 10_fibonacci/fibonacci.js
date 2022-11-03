5ae1fd5761e5054cba18f3c2037a342a743cb60b
clairemanthey <clairemanthey@gmail.com>
2022-11-02 23:38:41 -0500

    added my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let arr = [0, 1];
+	if(num < 0){
+		return "OOPS"
+	}else{
+		for (let i = 2; i < num + 1; i++){
+			arr.push(arr[i - 2] + arr[i - 1])
+	    }
+	    return arr[num];
+	}
 
 };
 
