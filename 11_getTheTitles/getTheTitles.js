dc1b507b2cdf66051c5992e7c7429616c81e8e47
clairemanthey <clairemanthey@gmail.com>
2022-11-07 18:05:12 -0600

    added my solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = bookList => {
+	return bookList.map(book => book.title)
+}
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
